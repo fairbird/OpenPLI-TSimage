559ee4c7ca647cf39b261401474ecff233e24ea9
RAED <rrrr53@hotmail.com>
2017-04-26 01:42:11 +0300

    Update dvb.cpp

--- lib/dvb/dvb.cpp
+++ lib/dvb/dvb.cpp
@@ -333,7 +333,6 @@ eDVBUsbAdapter::eDVBUsbAdapter(int nr)
 	if (ioctl(frontend, FE_GET_PROPERTY, &props) < 0)
 		eDebug("[eDVBUsbAdapter] FE_GET_PROPERTY DTV_ENUM_DELSYS failed %m");
 
-#endif
 	::close(frontend);
 	frontend = -1;
 
@@ -424,7 +423,6 @@ eDVBUsbAdapter::eDVBUsbAdapter(int nr)
 	ioctl(vtunerFd, VTUNER_SET_FE_INFO, &fe_info);
 	if (prop[0].u.buffer.len > 0)
 		ioctl(vtunerFd, VTUNER_SET_DELSYS, prop[0].u.buffer.data);
-#endif
 	ioctl(vtunerFd, VTUNER_SET_HAS_OUTPUTS, "no");
 	ioctl(vtunerFd, VTUNER_SET_ADAPTER, nr);
 
