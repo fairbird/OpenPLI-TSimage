8cf85e96b8c39de7915d9464ee7398c15dcb1774
RAED <rrrr53@hotmail.com>
2017-05-25 16:50:16 +0300

    Add -force-overwrite to install packages from feed

--- lib/python/Screens/PluginBrowser.py
+++ lib/python/Screens/PluginBrowser.py
@@ -228,7 +228,7 @@ class PluginDownloadBrowser(Screen):
          'back': self.requestClose})
         if os.path.isfile('/usr/bin/opkg'):
             self.ipkg = '/usr/bin/opkg'
-            self.ipkg_install = self.ipkg + ' install'
+            self.ipkg_install = self.ipkg + ' install -force-overwrite'
             self.ipkg_remove = self.ipkg + ' remove --autoremove'
         else:
             self.ipkg = 'ipkg'
