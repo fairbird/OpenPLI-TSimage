aadf974737fa7e5110f71069a9cbd9ec84cef307
RAED <rrrr53@hotmail.com>
2017-06-26 15:57:55 +0300

    remove installation date, as we don't know how to reliably determine that

--- lib/python/Screens/About.py
+++ lib/python/Screens/About.py
@@ -38,7 +38,8 @@ class About(Screen):
 
         AboutText += _('Hardware: ') + about.getHardwareTypeString() + '\n'
         AboutText += _('CPU: ') + about.getCPUInfoString() + '\n'
-        AboutText += _('Installed: ') + about.getFlashDateString() + '\n'
+        # [WanWizard] Removed until we find a reliable way to determine the installation date
+	# AboutText += _("Installed: ") + about.getFlashDateString() + "\n"
         AboutText += _('Kernel version: ') + about.getKernelVersionString() + '\n'
 
         EnigmaVersion = 'Enigma: ' + about.getEnigmaVersionString()
