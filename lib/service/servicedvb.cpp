ef3e8bdacdd14fd2aac66d7366929740b800485b
RAED <rrrr53@hotmail.com>
2017-08-26 14:16:51 +0300

    Revert PiP: adjustments required for making the quad PiP feature working

--- lib/service/servicedvb.cpp
+++ lib/service/servicedvb.cpp
@@ -1043,8 +1043,7 @@ eDVBServicePlay::eDVBServicePlay(const eServiceReference &ref, eDVBService *serv
 	m_cutlist_enabled(1),
 	m_subtitle_widget(0),
 	m_subtitle_sync_timer(eTimer::create(eApp)),
-	m_nownext_timer(eTimer::create(eApp)),
-	m_qpip_mode(false)
+	m_nownext_timer(eTimer::create(eApp))
 {
 	CONNECT(m_service_handler.serviceEvent, eDVBServicePlay::serviceEvent);
 	CONNECT(m_service_handler_timeshift.serviceEvent, eDVBServicePlay::serviceEventTimeshift);
@@ -2151,7 +2150,7 @@ int eDVBServicePlay::selectAudioStream(int i)
 
 	m_current_audio_pid = apid;
 
-	if ((!m_qpip_mode || !m_noaudio) && m_decoder->setAudioPID(apid, apidtype))
+	if (m_decoder->setAudioPID(apid, apidtype))
 	{
 		eDebug("[eDVBServicePlay] set audio pid %04x failed", apid);
 		return -4;
@@ -2854,7 +2853,7 @@ void eDVBServicePlay::updateDecoder(bool sendSeekableStateChanged)
 	if (m_decoder)
 	{
 		bool wasSeekable = m_decoder->getVideoProgressive() != -1;
-		if (!m_noaudio && !m_qpip_mode)
+		if (!m_noaudio)
 		{
 			if (m_dvb_service)
  			{
@@ -3524,22 +3523,6 @@ ePtr<iStreamData> eDVBServicePlay::getStreamingData()
 		retval = new eDVBServicePMTHandler::eStreamData(program);
 	}
 	return retval;
-}
-
-void eDVBServicePlay::setQpipMode(bool value, bool audio)
-{
-	m_qpip_mode = value;
-	m_noaudio = !audio;
-
-	if(m_decoder)
-	{
-		if (!m_noaudio)
-			selectAudioStream();
-		else
-			m_decoder->setAudioPID(-1, -1);
-
-		m_decoder->set();
-	}
 }
 
 DEFINE_REF(eDVBServicePlay)
