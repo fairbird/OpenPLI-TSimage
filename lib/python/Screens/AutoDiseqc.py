a476f52f56ace91b6840c8bcf78299ed82e376a3
RAED <rrrr53@hotmail.com>
2017-10-01 12:44:43 +0300

    Added AutoDiseqc for Eutelsat 16

--- lib/python/Screens/AutoDiseqc.py
+++ lib/python/Screens/AutoDiseqc.py
@@ -100,7 +100,26 @@ class AutoDiseqc(Screen, ConfigListScreen):
 			318,
 			"Hotbird 13.0e"),
 
-		# thor  3592 CT24
+		# eutelsat 160 hrt
+ 		(
+ 			10721,
+ 			27500,
+ 			eDVBFrontendParametersSatellite.Polarisation_Horizontal,
+ 			eDVBFrontendParametersSatellite.FEC_3_4,
+ 			eDVBFrontendParametersSatellite.Inversion_Off,
+ 			160,
+ 			eDVBFrontendParametersSatellite.System_DVB_S,
+ 			eDVBFrontendParametersSatellite.Modulation_Auto,
+ 			eDVBFrontendParametersSatellite.RollOff_auto,
+ 			eDVBFrontendParametersSatellite.Pilot_Unknown,		
+ 			-1,
+ 			0,
+ 			1,
+ 			20100,
+ 			366,
+ 			"Eutelsat 16A 16.0e"),
+ 
+  		# thor  3592 CT24
 		(
 			12072,
 			28000,
